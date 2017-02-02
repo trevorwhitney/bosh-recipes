a561e9f33be3b86d2e54ff3716097c338ca405a0
Trevor Whitney <trevorjwhitney@gmail.com>
2017-02-02 00:29:04 +0000

    deploy concourse script and fixes to concourse manifest

---
+++ gcp/bin/deploy-concourse.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+bin_dir="$(cd $(dirname "$0") && pwd)"
+source $bin_dir/setup_environment.sh
+
+export external_ip=`gcloud compute addresses describe concourse | grep ^address: | cut -f2 -d' '`
+export director_uuid=`bosh status --uuid 2>/dev/null`
+export common_password=`ruby -ryaml -e "puts YAML.load_file('privates/concourse-credentials.yml')['credentials']['common_password']"`
+export atc_password=`ruby -ryaml -e "puts YAML.load_file('privates/concourse-credentials.yml')['credentials']['atc_password']"`
+
+bosh deployment concourse.yml
+bosh -n deploy
