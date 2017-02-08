56bed0ea331fbc63ff4198ce1be039da9cc6ebe2
Trevor Whitney <trevorjwhitney@gmail.com>
2017-02-08 19:43:00 +0000

    working bosh director deployment

--- bin/deploy-bosh-director.sh
+++ bin/deploy-bosh-director.sh
@@ -16,21 +16,31 @@ if [ ! -e ~/.ssh/bosh ]; then
     sshKeys=<( gcloud compute project-info describe --format=json | jq -r '.commonInstanceMetadata.items[] | select(.key == "sshKeys") | .value' & echo "bosh:$(cat ~/.ssh/bosh.pub)"  )
 fi
 
-# $bin_dir/generate-bosh-manifest.sh $1
-# bosh-init deploy bosh.yml
-# gsutil cp ${privates_dir}/${project_id}/bosh-credentials.yml gs://${project_id}-terraform-config
-# gsutil cp bosh-state.json gs://${project_id}-terraform-config
+export admin_username=$1
+export service_account=bosh-user
+export service_account_email=${service_account}@${project_id}.iam.gserviceaccount.com
+mkdir -p ${privates_dir}/${project_id}
 
-bosh -e gcp -d bosh interpolate bosh.yml \
-  -v "project_id=${project_id}"
+bosh -e gcp -d bosh create-env bosh.yml \
+  -v "project_id=${project_id}" \
   -v "zone=${zone}" \
   -v "service_account_email=${service_account_email}" \
   -v "network=${network}" \
   -v "subnetwork=${subnetwork}" \
   -v "admin_username=${admin_username}" \
   -v "ssh_key_path=${ssh_key_path}" \
-  --vars-store="${privates_dir}/${project_id}/bosh-deployment-vars.yml" \
-  bosh-final.yml
+  --vars-store="${privates_dir}/${project_id}/bosh-deployment-vars.yml"
 
 gsutil cp ${privates_dir}/${project_id}/bosh-deployment-vars.yml gs://${project_id}-terraform-config
 gsutil cp bosh-state.json gs://${project_id}-terraform-config
+
+mkdir -p $privates_dir/$project_id/certs
+erb $gcp_dir/rootCA.pem.erb > $privates_dir/$project_id/certs/rootCA.pem
+erb $gcp_dir/admin_password.txt.erb > $privates_dir/$project_id/admin_password.txt
+
+echo "Targeting bosh director. Use the following credentials when prompted"
+echo "username: $admin_username"
+echo "password: $(cat $privates_dir/$project_id/admin_password.txt)"
+bosh -e https://10.0.0.6 --ca-cert privates/twhitney-bosh/certs/rootCA.pem alias-env gcp
+bosh -e gcp login
+
